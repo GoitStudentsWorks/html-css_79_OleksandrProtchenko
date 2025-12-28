5f60b3134231437ec0565850182be44bb2b8b043
Ivon Lagta <islaguta@gmail.com>
2025-12-29 00:19:43 +0100

    made mobile header and modal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-burgerBtn.classList.toggle('active');
+const btn = document.getElementById('burgerBtn');
+const box = document.getElementById('modalMenu');
+btn.addEventListener('click', () => {
+  const isActive = btn.classList.toggle('active');
+  box.classList.toggle('is-open');
+});
