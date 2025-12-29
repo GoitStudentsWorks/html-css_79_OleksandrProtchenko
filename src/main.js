a6e3f5717ff1fbe0717b14ea8725f5a65da5d234
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2025-12-29 11:06:07 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
-const btn = document.getElementById('burgerBtn');
-const box = document.getElementById('modalMenu');
-btn.addEventListener('click', () => {
-  const isActive = btn.classList.toggle('active');
-  box.classList.toggle('is-open');
-});
+
